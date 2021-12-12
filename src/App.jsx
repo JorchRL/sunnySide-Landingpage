b4ec63e1c7f81d85fe5589e40c3d4a3bf72324c5
Jorch <jorch.rl@gmail.com>
2021-12-12 00:42:25 -0600

    Add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import "./app.scss";
 const App = () => {
   return (
+      {/* HERO */}
+      <header className='hero'>
+        <div className='navbar flex'>
+          <h2 className='logo'>sunnyside</h2>
+          <div className='menuContainer'>
+            <ul>
+              <li>About</li>
+              <li>Services</li>
+              <li>Projects</li>
+              <li>CONTACT</li>
+            </ul>
+          </div>
+        </div>
+        <div className='title'>
+          <h2>WE ARE CREATIVES</h2>
+          <div className='arrow'></div>
+        </div>
+      </header>
       </section>
       <section className='testimonials flex'></section>
       <section className='pics grid'></section>
