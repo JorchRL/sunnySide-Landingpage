cb43156d31ba3c9a2197519edccbf62fa13547b2
Jorch <jorch.rl@gmail.com>
2021-12-12 00:43:10 -0600

    Add Info cards section

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,29 @@ const App = () => {
           <div className='arrow'></div>
         </div>
       </header>
+      {/* INFO CARDS */}
+      <section className='info flex'>
+        <div className='tile text'>
+          <h2>Transform your brand</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            suscipit voluptates, consequuntur aspernatur ab quidem cum aliquid
+            officiis ad vel nisi rem nihil. Ab consequuntur nisi incidunt fuga
+            nihil officiis.
+          </p>
+        </div>
+        <div className='tile img' id='eggImg'></div>
+        <div className='tile img' id='cupImg'></div>
+        <div className='tile text'>
+          <h2>Stand out to the right audience</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+            corporis, facere illo cupiditate libero soluta, cum et iste eveniet
+            doloremque vitae? Cupiditate quas est quam quisquam reiciendis
+            praesentium beatae fugit.
+          </p>
+        </div>
+      </section>
       </section>
       <section className='testimonials flex'></section>
       <section className='pics grid'></section>
