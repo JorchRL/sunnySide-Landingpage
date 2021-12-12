c42a555d9da58dc18639fb9e610e82b93d9d101a
Jorch <jorch.rl@gmail.com>
2021-12-12 00:41:04 -0600

    Add sections to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import "./app.scss";
-function App() {
+const App = () => {
   return (
-    <div className=''>
-      <h1>SunnySide</h1>
-    </div>
-  );
-}
-
-export default App;
+      </section>
+      <section className='testimonials flex'></section>
+      <section className='pics grid'></section>
+      <footer class='footer'></footer>
